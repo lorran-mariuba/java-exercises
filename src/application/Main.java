b7c37ed7033e512e96c7d7843045bd67c4d7c94d
Lorran Mariuba de Oliveira <llorrann@gmail.com>
2021-03-23 00:31:45 -0400

    Exercise about constructor.

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,50 +3,38 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
-import entities.Account;
+import entities.Product;
 
 public class Main {
 
 	public static void main(String[] args) {
-
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		Account account;
+				
+		System.out.println("Enter product data: ");
+		System.out.print("Name: ");
+		String name = sc.nextLine();
+		System.out.print("Price: ");
+		double price = sc.nextDouble();
 
-		System.out.print("Enter account number: ");
-		int number = sc.nextInt();
-		System.out.print("Enter account holder: ");
-		sc.nextLine();
-		String holder = sc.nextLine();
-		System.out.print("Is there an initial deposit (y/n)? ");
-		char response = sc.next().charAt(0);
-		if (response == 'y') {
-			System.out.print("Enter initial deposit value: ");
-			double initialDeposit = sc.nextDouble();
-			account = new Account(number, holder, initialDeposit);
-		}
-		else {
-			account = new Account(number, holder);
-		}
+		Product product = new Product(name, price); 
 		
 		System.out.println();
-		System.out.println("Account data:");
-		System.out.println(account);
-		
+		System.out.println("Product data: " + product);
 		System.out.println();
-		System.out.print("Enter a deposit value: ");
-		double depositValue = sc.nextDouble();
-		account.deposit(depositValue);
-		System.out.println("Updated account data:");
-		System.out.println(account);
-		
+		System.out.print("Enter the number of products to be added in stock: ");
+		int quantity = sc.nextInt();
+		product.addProducts(quantity);
 		System.out.println();
-		System.out.print("Enter a withdraw value: ");
-		double withdrawValue = sc.nextDouble();
-		account.withdraw(withdrawValue);
-		System.out.println("Updated account data:");
-		System.out.println(account);
-		
+		System.out.println("Updated data: " + product);
+		System.out.println();
+		System.out.print("Enter the number of products to be removed from stock: ");
+		quantity = sc.nextInt();
+		product.removeProducts(quantity);
+		System.out.println();
+		System.out.println("Updated data: " + product);
 		sc.close();
+
 	}
+
 }
